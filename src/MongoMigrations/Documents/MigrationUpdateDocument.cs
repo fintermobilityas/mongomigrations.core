c9f54912d8131f2a6e301f44150f8c8bf4303ac9
Peter Rekdal Sunde <peters@users.noreply.github.com>
2018-08-23 14:36:18 +0200

    R#

--- src/MongoMigrations/Documents/MigrationUpdateDocument.cs
+++ src/MongoMigrations/Documents/MigrationUpdateDocument.cs
@@ -10,24 +10,15 @@ namespace MongoMigrations.Documents
 {
     public sealed class MigrationUpdateDocument : IWriteModel
     {
-        readonly FilterDefinition<BsonDocument> _filterDefinition;
-        readonly UpdateDefinition<BsonDocument> _updateDefinition;
-
         public WriteModel<BsonDocument> Model { get; }
         [UsedImplicitly] public string JsonDocument => BsonDocument?.ToJson(new JsonWriterSettings { OutputMode = JsonOutputMode.Strict });
         [UsedImplicitly] public BsonDocument BsonDocument => Model?.ToWriteModelBsonDocument();
 
         public MigrationUpdateDocument([NotNull] FilterDefinition<BsonDocument> filterDefinition, [NotNull] UpdateDefinition<BsonDocument> updateDefinition)
         {
-            _updateDefinition = updateDefinition ?? throw new ArgumentNullException(nameof(updateDefinition));
-            _filterDefinition = filterDefinition ?? throw new ArgumentNullException(nameof(filterDefinition));
-            Model = new UpdateOneModel<BsonDocument>(_filterDefinition, _updateDefinition);
-        }
-
-        internal MigrationUpdateDocument Combine([NotNull] Func<UpdateDefinitionBuilder<BsonDocument>, UpdateDefinition<BsonDocument>> builder)
-        {
-           var updateDefinition = Builders<BsonDocument>.Update.Combine(_updateDefinition, builder(Builders<BsonDocument>.Update));
-           return new MigrationUpdateDocument(_filterDefinition, updateDefinition);
+            if (filterDefinition == null) throw new ArgumentNullException(nameof(filterDefinition));
+            if (updateDefinition == null) throw new ArgumentNullException(nameof(updateDefinition));
+            Model = new UpdateOneModel<BsonDocument>(filterDefinition, updateDefinition);
         }
     }
 }
