3779a5e24b9f2e48d688aaf8da4c1843c859c36d
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-02-06 13:12:25 +0100

    Add test project.

---
+++ MongoMigrations.Tests/BaseFixture.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+
+namespace MongoMigrations.Tests
+{
+    public sealed class BaseFixture
+    {
+
+    }
+}
---
+++ MongoMigrations.Tests/MigrationLocatorTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace MongoMigrations.Tests
+{
+    public class MigrationLocatorTests : IClassFixture<BaseFixture>
+    {
+        readonly BaseFixture _baseFixture;
+        readonly MigrationLocator _migrationLocator;
+
+        public MigrationLocatorTests(BaseFixture baseFixture)
+        {
+            _baseFixture = baseFixture;
+            _migrationLocator = new MigrationLocator();
+        }
+
+        [Fact]
+        public void TestLookForMigrationsInAssemblyOfType()
+        {
+            _migrationLocator.LookForMigrationsInAssembly(typeof(MigrationLocatorTests).Assembly);
+
+            var migrations = _migrationLocator.GetAllMigrations();
+            Assert.Single(migrations);
+        }
+    }
+
+    class TestMigration01 : Migration
+    {
+        public TestMigration01() : base(0)
+        {
+        }
+
+        public override void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
