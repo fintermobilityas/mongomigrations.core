05d8be6f4db07498c46625fa4604777229db6f20
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-02-07 09:17:12 +0100

    Add version property.

--- src/MongoMigrations/ConcurrentMigrationException.cs
+++ src/MongoMigrations/ConcurrentMigrationException.cs
@@ -4,9 +4,11 @@ namespace MongoMigrations
 {
     public class ConcurrentMigrationException : MigrationException
     {
+        public MigrationVersion Version { get; }
+
         public ConcurrentMigrationException(MigrationVersion version, Exception innerException) : base($"Migration is already in progress. Version: {version}", innerException)
         {
-
+            Version = version;
         }
     }
 }
